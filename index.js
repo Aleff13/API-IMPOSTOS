022a3024182a4ed4da6c19d8de8cbc2456ff0bf7
aleff menna resler <83561751+Aleff13@users.noreply.github.com>
2022-03-12 16:24:58 -0300

    I fixed the bugs.
    I create a new route for icms complete.
    
    The tax calc now is running.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const routes = require("./routes")
+const Tax = require("./taxCalc")
 
 const app = express()
 
@@ -15,6 +16,8 @@ app.use(express.json())
 app.use(cors())
 
 app.use(routes)
+
+app.use(Tax)
 
 app.listen(21262, () => {
 
--- routes.js
+++ routes.js
@@ -3,12 +3,29 @@ const { json } = require("body-parser")
 const express = require("express")
 const routes = express.Router()
 const fs = require("fs")
+const Tax = require("./taxCalc")
 
-routes.get("/consult/icms/simple", (req, res) => {
+routes.get("/consult/icms-simple", (req, res) => {
 
     const body = req.body
+
+    let calculator = new Tax()
     
-    return res.json(body).status(200)
+    let resultado = calculator.icms(body.price, body.mva, body.internal, body.external)
+
+    return res.json("O valor de icms a ser pago é de: " + resultado).status(200)
+
+})
+
+routes.get("/consult/icms-complete", (req, res) => {
+
+    const body = req.body
+
+    let calculator = new Tax()
+    
+    let resultado = calculator.icms(body.price, body.mva, body.internal, body.external, body.descount, body.shipping, body.ipi)
+
+    return res.json(resultado).status(200)
 
 })
 
